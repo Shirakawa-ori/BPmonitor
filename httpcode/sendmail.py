22bd44d219699560d1f70993abd5d9c5f5a61bac
Shirakawa <673254896@qq.com>
2017-09-01 16:23:37 +0800

    Update sendmail.py

--- httpcode/sendmail.py
+++ httpcode/sendmail.py
@@ -19,24 +19,23 @@ except BaseException:
     exit(0)
 
 #第三方 SMTP 服务
-mail_host="smtp.mxhichina.com"		#设置服务器
-mail_user="your@mail"							#用户名
-mail_pass="yourpwd"								#口令 
- 
-sender = 'your@mail'							#发送邮箱
+mail_host="smtp.mxhichina.com"			#设置smtp服务器
+mail_user="your@mail"					#用户名
+mail_pass="yourpwd"						#密码 
+sender = 'your@mail'					#发送邮箱
 receivers = ['your@mail2']				#接收邮件邮箱
 
 # 三个参数：第一个为文本内容，第二个 html 设置网页格式，第三个 utf-8 设置编码
 message = MIMEText("<html>" + mail_smg + "</html>", 'html', 'utf-8')
-message['From'] = Header("From", 'utf-8')								#发件人
-message['To'] =  Header("To", 'utf-8')									#收件人
-subject = '服务不可达报告'																#邮件主题
-message['Subject'] = Header(subject, 'utf-8')						#邮件正文
+message['From'] = Header("From", 'utf-8')		#发件人
+message['To'] =  Header("To", 'utf-8')			#收件人
+subject = '服务不可达报告'						#邮件主题
+message['Subject'] = Header(subject, 'utf-8')	#邮件正文
 
 #发送邮件
 try:
     smtpObj = smtplib.SMTP() 
-    smtpObj.connect(mail_host, 25)											#25为SMTP端口号
+    smtpObj.connect(mail_host, 25)				#25为SMTP端口号
     smtpObj.ehlo()
     smtpObj.login(mail_user,mail_pass)  
     smtpObj.sendmail(sender, receivers, message.as_string())
